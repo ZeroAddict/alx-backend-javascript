300e89419db36bf0a947fdcd6ed227542a841408
ZeroAddict <yemifrancisolayemi@gmail.com>
2024-10-05 02:54:02 +0000

    aDDED TASKS 7 - 101

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+const cloneCar = Symbol('cloneCar');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  [cloneCar]() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+
+  cloneCar() {
+    return this[cloneCar]();
+  }
+}
